71914455fd109f5c112b698bd53cd2b17b084c38
Román Pozzo <rhpozzo@gmail.com>
2020-02-16 22:39:23 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Moment from 'react-moment';
 import 'moment-timezone';
+import moment from 'moment-timezone';
 
 const App = () => {
-  return(
-    <div>
-      <Moment format="DD/MM/YYYY"></Moment>
-
-
-
-      
+    let time = new Date
+    let hours = time.getHours();
+    let minutes = setInteval(() => {
+        time.getMinutes()
+    },1000);
+    let seconds = time.getSeconds();
+    return(
+    <div className='row'>
+        <h1 className='dateBox'>Station Time: {minutes}</h1>
     </div>
+
   )
 }
 
